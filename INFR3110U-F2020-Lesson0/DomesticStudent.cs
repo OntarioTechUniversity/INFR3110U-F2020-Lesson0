b8e09c301bb515d40bab3ebe7c54895241bbe3f6
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-08-31 13:26:07 -0400

    added class diagram

---
+++ INFR3110U-F2020-Lesson0/DomesticStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INFR3110U_F2020_Lesson0
+{
+    public class DomesticStudent: Student
+    {
+        public string DomesticAddress
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
